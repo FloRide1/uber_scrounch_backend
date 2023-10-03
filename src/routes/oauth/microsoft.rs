96bf8653721da4d44507c7089f3f78ba291877b6
FloRide1 <florian.reimat@orange.fr>
2023-10-03 21:51:45 +0200

    [OAUTH] Fix microsoft scope in oauth

--- src/routes/oauth/microsoft.rs
+++ src/routes/oauth/microsoft.rs
@@ -5,9 +5,7 @@ use oauth2::basic::BasicClient;
 pub async fn microsoft_auth(State(client): State<BasicClient>) -> impl IntoResponse {
     let (auth_url, _csrf_token) = client
         .authorize_url(oauth2::CsrfToken::new_random)
-        .add_scope(oauth2::Scope::new("openid".to_string()))
-        .add_scope(oauth2::Scope::new("email".to_string()))
-        .add_scope(oauth2::Scope::new("profile".to_string()))
+        .add_scope(oauth2::Scope::new("User.ReadBasic.All".to_string()))
         .url();
 
     Redirect::to(auth_url.as_ref())
